c3bf1216e436526dbac46e5898af7d46eb857e97
Edgar Sandoval Developer <59340481+edgarsandovaldev@users.noreply.github.com>
2023-05-01 17:08:07 -0500

    Creqacion dell login servicio de logica y llamado a api

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
+
 const routes: Routes = [
-  { path: '', component: LoginComponent }
+  { path: '', component: LoginComponent },
+
 ];
 
 @NgModule({
